da479ec89eaf18245614420d206a758eeffd3197
rachele33 <rdstwin2@hotmail.com>
2021-09-30 13:39:20 +0100

    we changed hi to hello and added new question

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi('Rachel')
+
+    print("how are you doing?")
+    print("where do you live?")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
