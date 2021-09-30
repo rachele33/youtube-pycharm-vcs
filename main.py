0e0a49b39760d19c3fe0b320476a5e4c569b33f3
rachele33 <rdstwin2@hotmail.com>
2021-09-30 13:52:08 +0100

    testing new function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+twdef my_function(a,b):
+    return a+b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -12,4 +14,6 @@ if __name__ == '__main__':
     print("how are you doing?")
     print(password)
 
+
+    print (my_function(2,4))
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
